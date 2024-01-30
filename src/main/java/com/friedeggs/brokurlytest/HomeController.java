d3ebdbc5c492e517e3c5902d98f9c02b05b5e355
sngbmYu <74458310+sngbmYu@users.noreply.github.com>
2024-01-30 14:39:11 +0900

    HomeController에 testMethod 추가 (#2)
    승인 받아서 merge 하겠음

--- src/main/java/com/friedeggs/brokurlytest/HomeController.java
+++ src/main/java/com/friedeggs/brokurlytest/HomeController.java
@@ -1,5 +1,14 @@
 package com.friedeggs.brokurlytest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
     // HomeController 주석 추가
+
+    public void testMethod() {
+
+    }
 }
