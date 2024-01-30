f315cc969e8ebdbffa623a9727bba7d1fc93bd59
eunoia73 <soo97703@naver.com>
2024-01-30 16:49:26 +0900

    바꿨당

--- src/main/java/com/friedeggs/brokurlytest/UnoController.java
+++ src/main/java/com/friedeggs/brokurlytest/UnoController.java
@@ -4,5 +4,7 @@ package com.friedeggs.brokurlytest;
 public class UnoController {
     public static void main(String[] args) {
         System.out.println("Hello broKurly");
+        System.out.println("hi");
     }
+
 }
