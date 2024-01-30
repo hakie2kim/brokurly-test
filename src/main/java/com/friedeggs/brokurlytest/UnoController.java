99970fb571f90db378e7f09bf079f636ec4eb83b
aaetom <omniboy13sl@gmail.com>
2024-01-30 16:49:22 +0900

    error

--- src/main/java/com/friedeggs/brokurlytest/UnoController.java
+++ src/main/java/com/friedeggs/brokurlytest/UnoController.java
@@ -4,5 +4,6 @@ package com.friedeggs.brokurlytest;
 public class UnoController {
     public static void main(String[] args) {
         System.out.println("Hello broKurly");
+        System.out.println("change");
     }
 }
