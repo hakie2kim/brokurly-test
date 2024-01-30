75373530e6be732ccd74cc4d3d7910c590876b53
hakjun <hakie2kim@gmail.com>
2024-01-30 12:36:25 +0900

    fix: comment test - 주석 테스트입니다.

--- src/main/java/com/friedeggs/brokurlytest/BrokurlytestApplication.java
+++ src/main/java/com/friedeggs/brokurlytest/BrokurlytestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BrokurlytestApplication {
-
+    // 주석 테스트입니다.
     public static void main(String[] args) {
         SpringApplication.run(BrokurlytestApplication.class, args);
     }
