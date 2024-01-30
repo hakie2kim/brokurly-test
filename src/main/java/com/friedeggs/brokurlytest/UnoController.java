cef134fd2d98933c0289eceff569149c9a173132
aaetom <omniboy13sl@gmail.com>
2024-01-30 16:56:00 +0900

    error2

--- src/main/java/com/friedeggs/brokurlytest/UnoController.java
+++ src/main/java/com/friedeggs/brokurlytest/UnoController.java
@@ -5,5 +5,6 @@ public class UnoController {
     public static void main(String[] args) {
         System.out.println("Hello broKurly");
         System.out.println("change");
+        System.out.println("change2");
     }
 }
