61bfa262137fc35e1e79916ccfb5144bfd6012de
hakjun <hakie2kim@gmail.com>
2024-01-30 12:30:12 +0900

    feat: first commit - 커밋 메시지입니다.

---
+++ src/main/java/com/friedeggs/brokurlytest/BrokurlytestApplication.java
@@ -0,0 +1,13 @@
+package com.friedeggs.brokurlytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokurlytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokurlytestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/friedeggs/brokurlytest/BrokurlytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friedeggs.brokurlytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokurlytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
